09ee45d883ea7b77dc6e3dc4b1e0e45c9097c670
Arne Van Den Kerchove <arne@vandenkerchove.com>
2023-09-12 20:27:02 +0200

    Facebook embed

--- brainbrowsr/ui/assets/js/app.js
+++ brainbrowsr/ui/assets/js/app.js
@@ -11,12 +11,23 @@ const CALIBRATION_EVENT_DELAY = 1000;
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 io.subscribe('events');
-io.on('events', handlePredict);
+io.on('events', handleEvent);
 
+function handleEvent(data,meta){
+	for (const key of Object.keys(data)){
+		const eventType = data[key]['label']
+		if (eventType === 'predict'){
+			const prediction = JSON.parse(data[key]['data'])['result'];
+			for (const stim of stimuli){
+				stim.style.border = 'initial';
+				if (stim.id === prediction){
+					stim.style.border = '5px solid green';
+					stim.click()
+				}
 
-async function handlePredict(data,meta){
-	console.log(data);
-	console.log(meta);
+			}
+		}
+	}
 }
 
 async function calibrate(){
@@ -27,11 +38,36 @@ async function calibrate(){
 		for (const stim of perm) {
 			console.log("Calibrating target " + stim.id);
 			stim.style.border = '5px solid yellow';
-			await delay(CALIBRATION_EVENT_DELAY);
+			await delay(CALIBRATION_DURATION);
+			console.log(stim);
 			io.event('stim', stim.id)
-			await delay(CALIBRATION_DURATION-CALIBRATION_EVENT_DELAY);
+			await delay(CALIBRATION_EVENT_DELAY);
 			stim.style.border = 'initial';
 		}
 	}
+	console.log('========== Stop calibration ==========');
 	io.event('calibration_stop', 101);
+}
+
+const contentFrame = document.getElementByClassName('_10b4')[0];
+
+var powerState = false;
+function selectUp() {
+	console.log('up');
+    //myIframe.contentWindow.scrollTo(xcoord,ycoord);
+}
+function selectDown(){
+	console.log('down');
+}
+function selectOn(){
+	if(powerState){
+		console.log('off');
+		powerState = false;
+	}else{
+		console.log('on');
+		powerState = true;
+	}
+}
+function selectLike(){
+	console.log('like');
 }
